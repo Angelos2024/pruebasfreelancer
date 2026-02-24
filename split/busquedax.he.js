fb9e0e0e1bf6095529a8fb5dfd1fdac5efacc4cc
Angelos2024 <skyblackroan@gmail.com>
2026-02-23 20:36:48 -0600

    update

--- split/busquedax.he.js
+++
@@ -1,9 +0,0 @@
-/* split: he */
-   function normalizeHebrew(text) {
-     return String(text || '')
-        .replace(/[\u200C-\u200F\u202A-\u202E]/g, '')
-       .replace(/[\u0591-\u05BD\u05BF\u05C1-\u05C2\u05C4-\u05C7]/g, '')
-.replace(/[\s\u05BE\-\u2010-\u2015\u2212]/g, '')
-       .replace(/[׃.,;:!?()"“”'׳״]/g, '');
-   }
- 
